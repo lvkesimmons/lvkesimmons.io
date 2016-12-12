7e0106ef871537aca5fa77b7035b7da1662a5822
Luke Simmons <lvkesimmons@gmail.com>
2016-12-12 00:50:36 -0500

    Add files via upload

--- js/skia.js
+++ js/skia.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     
 $('#fullpage').fullpage({
-    anchors:['skiaTitle1', 'skiaIntro1', 'skiaProblem1', 'skiaDirection1', 'skiaSketching1', 'skiaModeling1', 'skiaCamera1', 'skiaColors1', 'skiaApp1', 'skiaAppLayout1'],
+    anchors:['skiaTitle1', 'skiaIntro1', 'skiaProblem1', 'skiaDirection1', 'skiaSketching1', 'skiaModeling1', 'skiaCamera1', 'skiaColors1', 'skiaApp1', 'skiaAppLayout1', 'skiaProductPage1'],
     navigation: true,
     navigationPosition: 'right',
-    navigationTooltips: ['SKIA', 'INTRO', 'PROBLEM', 'DIRECTION 1', 'SKETCHING', 'MODELING', 'CAMERA', 'COLORS', 'APP', 'LAYOUT'],
+    navigationTooltips: ['SKIA', 'INTRO', 'PROBLEM', 'DIRECTION 1', 'SKETCHING', 'MODELING', 'CAMERA', 'COLORS', 'APP', 'LAYOUT', 'PRODCUT PAGE'],
     showActiveTooltip: false,
     css3: true,
     scrollingSpeed: 1000,
