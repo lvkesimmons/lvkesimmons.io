418b6e7d51be9ece2ecda86b973cebd80b8f4c5d
Luke Simmons <lvkesimmons@gmail.com>
2016-12-13 17:40:19 -0500

    Add files via upload

--- js/trihang.js
+++ js/trihang.js
@@ -1,10 +1,10 @@
 $(document).ready(function(){
     
 $('#fullpage').fullpage({
-    anchors:['triHangTitle', 'triHangIntro', 'triHangSketching', 'triHangScaleModels', 'triHangFullModels', 'triHangProcess', 'triHangPrototype', 'triHangTechnicalDrawing'],
+    anchors:['triHangTitle', 'triHangIntro', 'triHangSketching', 'triHangScaleModels', 'triHangFullModels', 'triHangProcess', 'triHangPrototype', 'triHangOrthographicDrawing'],
     navigation: true,
     navigationPosition: 'right',
-    navigationTooltips: ['TRI-HANG', 'INTRO', 'SKETCHING', 'SCALE MODELS', 'FULL SCALE MODELS', 'PROCESS', 'PROTOTYPE', 'TECHNICAL DRAWING'],
+    navigationTooltips: ['TRI-HANG', 'INTRO', 'SKETCHING', 'SCALE MODELS', 'FULL SCALE MODELS', 'PROCESS', 'PROTOTYPE', 'ORTHOGRAPHIC DRAWING'],
     showActiveTooltip: false,
     css3: true,
     scrollingSpeed: 1000,
