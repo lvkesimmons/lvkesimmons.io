287905c664948e1083182bd56cc04c025aa9e390
Luke Simmons <lvkesimmons@gmail.com>
2022-02-13 20:00:52 -0500

    Update tilt.js

--- js/tilt.js
+++ js/tilt.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         transition:     true,   // Set a transition on enter/exit.
         axis:           null,   // What axis should be disabled. Can be X or Y.
         reset:          true,   // If the tilt effect has to be reset on exit.
-        glare:          false,  // Enables glare effect
-        maxGlare:       .125       // From 0 - 1.
+        glare:          true,  // Enables glare effect
+        maxGlare:       .5       // From 0 - 1.
     });
 });
